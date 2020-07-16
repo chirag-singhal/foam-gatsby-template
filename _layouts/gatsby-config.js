c6beda5cd916efe09cec15a2b8aba5443a3214c7
chirag-singhal <csinghal208@gmail.com>
2020-07-16 23:24:58 +0530

    added support for math

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -8,13 +8,17 @@ module.exports = {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`, `.md`],
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          `gatsby-remark-mathjax`,
+        remarkPlugins: [
+          require('remark-math')
+        ],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-katex`,
+            options: {
+              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+              strict: `ignore`
+            }
+          },
         ],
       },
     },
